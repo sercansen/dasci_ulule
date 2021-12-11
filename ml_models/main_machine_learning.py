1a4371787b8e5cd1873a3d34bfef2d40ace531e1
Lucas <lucas.yakhontoff@gmail.com>
2021-12-11 21:11:30 +0100

    added multi layer perceptron to machine learning models

--- ml_models/main_machine_learning.py
+++ ml_models/main_machine_learning.py
@@ -1,16 +1,21 @@
 from pandas import DataFrame
+from ml_models import mlp
 
 
 def machine_learning(cat_name: str, data_general: DataFrame, data_pre_covid: DataFrame, data_post_covid: DataFrame, dict_df: dict) -> str:
 
+    to_print = """"""
     if cat_name == "tout":
         # On utilise les dataframe généraux comme "data_general" et tout le dictionnaire dict_df
-        pass
+        to_print += "<h2>Application d'un Multi Layer Perceptron</h2>"
+        to_print += mlp.mlp(data_general)
     else:
         # On utilise les dataframe spéficiques contenus dans dict_df avec les clefs suivante:
         data_cat_general = dict_df["data_" + cat_name]
         data_cat_pre_covid = dict_df["data_pre_covid_" + cat_name]
         data_cat_post_covid = dict_df["data_post_covid_" + cat_name]
 
-        pass
-    return ""
+        to_print += "<h2>Application d'un Multi Layer Perceptron</h2>"
+        to_print += mlp.mlp(data_general)
+
+    return to_print
